dacbf3c10480fea7fbed98d85911e9fed365b4bd
Aliyavar <aliyavarguluzada@gmail.com>
2024-07-16 12:35:31 +0400

    Logging with serilog

--- src/JobSearch.API/Program.cs
+++ src/JobSearch.API/Program.cs
@@ -4,32 +4,37 @@ using JobSearch.Application.Repositories;
 using JobSearch.Infrastructure;
 using JobSearch.Infrastructure.Repositories;
 using Microsoft.AspNetCore.Antiforgery;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Logging.ClearProviders();
+
+
+builder.Logging.AddSerilog();
+builder.Host.UseSerilog((context, configuration) =>
+              configuration.ReadFrom.Configuration(context.Configuration));
 
 builder.Services.AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddApiVersioning(option =>
 {
-    option.AssumeDefaultVersionWhenUnspecified = true; //This ensures if client doesn't specify an API version. The default version should be considered. 
-    option.DefaultApiVersion = new ApiVersion(1, 0); //This we set the default API version
-    option.ReportApiVersions = true; //The allow the API Version information to be reported in the client  in the response header. This will be useful for the client to understand the version of the API they are interacting with.
+    option.AssumeDefaultVersionWhenUnspecified = true;
+    option.DefaultApiVersion = new ApiVersion(1, 0);
+    option.ReportApiVersions = true;
 
     //------------------------------------------------//
     option.ApiVersionReader = ApiVersionReader.Combine(
-        new QueryStringApiVersionReader("api-version"));
-        //new HeaderApiVersionReader("X-Version"),
-        //new MediaTypeApiVersionReader("ver")); //This says how the API version should be read from the client's request, 3 options are enabled 1.Querystring, 2.Header, 3.MediaType. 
-                                               //"api-version", "X-Version" and "ver" are parameter name to be set with version number in client before request the endpoints.
-}).AddApiExplorer(options => {
-    options.GroupNameFormat = "'v'VVV"; //The say our format of our version number “‘v’major[.minor][-status]”
-    options.SubstituteApiVersionInUrl = true; //This will help us to resolve the ambiguity when there is a routing conflict due to routing template one or more end points are same.
-});
+        new QueryStringApiVersionReader("api-version"),
+        new MediaTypeApiVersionReader("ver"));
+
+}).AddApiExplorer(options =>
+   {
+       options.GroupNameFormat = "'v'VVV";
+       options.SubstituteApiVersionInUrl = true;
+   });
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
@@ -43,9 +48,16 @@ builder.Services.AddAntiforgery(options =>
     options.SuppressXFrameOptionsHeader = false;
 }); // should add antiforgery attribute to controllers
 
+
+
+
+
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseSerilogRequestLogging();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -61,6 +73,7 @@ else
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
 var antiforgery = app.Services.GetRequiredService<IAntiforgery>();
 
 app.Use((context, next) =>
@@ -79,5 +92,6 @@ app.Use((context, next) =>
 });
 
 app.MapControllers();
+
 
 app.Run();
